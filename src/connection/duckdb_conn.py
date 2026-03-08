4dfe7832460bea930f0ffad9b9ebfa95d0f27666
Maria Gomes <a79366@ualg.pt>
2026-03-08 02:58:17 +0000

    ci(Historico): Criando a base de dados com parquet para o projeto; Base de dados integrada e controller para ação de = também criado

--- src/connection/duckdb_conn.py
+++ src/connection/duckdb_conn.py
@@ -2,13 +2,29 @@ import duckdb
 
 class DatabaseCalculator():
     def init(self):
-        self.connect
+        self.connection()
     
-    def apagar_um_inserir():
+    def connection():
+        conn = duckdb.connect("historico.parquet")
+        conn.execute("""CREATE TABLE IF NOT EXISTS history ( 
+                    id BIGINT PRIMARY KEY, 
+                    expression TEXT NOT NULL,
+                    result TEXT NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );""")
+        conn.close()
+
+    def apagar_um_inserir(self):
         pass
 
-    def total_registro():
-        pass
+    def total_registro(self):
+        self.connection()
+        quantidade = conn.execute("""
+                                    SELECT COUNT(*) AS total
+FROM read_parquet('caminho/do/historico.parquet')
+                                """)
 
-    def inserir():
+        
+
+    def inserir(self):
         pass
